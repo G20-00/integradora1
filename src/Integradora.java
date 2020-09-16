a195ea5608e4c1f9ab73b9b19001a01b7a198f51
gianni benavides <sb5021087@gmail.com>
2020-09-16 16:46:58 -0500

    Modificacion en el primer metodo

--- src/Integradora.java
+++ src/Integradora.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Integradora{
 
-	public static final int CANTIDAD_MAXIMA = 100;
+	public static final int CANTIDAD_MAXIMA = 1000;
 	public static final String OBRA_NEGRA = "Obra negra";
 	public static final String OBRA_BLANCA = "Obra blanca";
 	public static final String OBRA_PINTURA = "Obra pintura";
@@ -10,5 +10,77 @@ public class Integradora{
 	public static void main (String [] args) {
 		Scanner scan = new Scanner(System.in);
 		boolean run = true;
+		String[] nombres = new String[CANTIDAD_MAXIMA];
+		String[] intencion = new String[CANTIDAD_MAXIMA];
+		double[] precios = new double[CANTIDAD_MAXIMA];
+		while(run) {
+			System.out.println("Escoja una opcion:");
+			System.out.println("1. Comparar una serie de productos y añadir a la lista el de menor precio.");
+			System.out.println("2. Mostar lista en pantalla.");
+			System.out.println("3. Finalizar programa.");
+			
+			int opcion =  Integer.parseInt(scan.nextLine());
+			switch(opcion) {
+			case 1: //compararProductos(scan, nombres, intencion, precios);
+					break;
+			case 2: //mostrarEnPantalla(intencion, nombres, precios);
+					//System.out.println();
+					//break;
+			case 3: run = false;
+					System.out.println("BYE BYE");
+					break;
+			default:System.out.println("Opcion no valida, por favor escoja una opcion correcta.");
+					System.out.println();
+			}
+		}
+		
+	
 	}	
+	public static void compararProductos(Scanner scan, String[] nombres, double[] precios) {
+		System.out.println("Compara los productos:");
+		System.out.println("Cuantos productos ingresaras para comparar?");
+		
+		int cantidad = Integer.parseInt(scan.nextLine());
+		double[] HomeCenter = new double[cantidad];
+		double[] FerreCentro = new double[cantidad];
+		double[] FerreteríaDelBarrio = new double[cantidad];
+		String[] compararNombre = new String[cantidad];
+		String[] compararIntencionalidad = new String[cantidad];
+		double precioUnitario= 0;
+		for(int i = 0; i<cantidad; i++) {
+			System.out.println("Ingrese el nombre del producto numero " + (i+1));	
+			compararNombre[i] = scan.nextLine();
+			
+			System.out.println("Cuantos unidades necesita del producto");
+			int Unidades = Integer.parseInt(scan.nextLine());
+			
+			System.out.println("Ingrese el precio del producto numero " + (i+1) +"En HomeCenter");
+			precioUnitario = Double.parseDouble(scan.nextLine());
+			HomeCenter[i] = precioUnitario * Unidades;
+			
+			System.out.println("Ingrese el precio del producto numero " + (i+1) +"Ferreteria del centro");
+			precioUnitario = Double.parseDouble(scan.nextLine());
+			FerreCentro[i] = precioUnitario * Unidades;
+			
+			System.out.println("Ingrese el precio del producto numero " + (i+1) +"Ferretería del barrio");
+			precioUnitario = Double.parseDouble(scan.nextLine());
+			FerreteríaDelBarrio[i] = precioUnitario * Unidades;
+			
+			
+			//compararIntencionalidad[i] =  obtenerIntecionalidad(scan);
+		}
+		
+		add(HomeCenter , FerreCentro , FerreteríaDelBarrio , compararNombre ,intencion,  nombres , precios);
+	}
+	public static void add(double[] HomeCenter, double[]FerreCentro , double[] FerreteríaDelBarrio ,String[] compararNombre, 
+			String[] intencion, String[] nombres, double[] precios) {
+		double menor = compararPrecio[0];
+		int posicionMenor=0;
+		for (int i = 1; i < compararPrecio.length; i++) {
+			if(compararPrecio[i]<menor) {
+				menor = compararPrecio[i];
+				posicionMenor = i;
+			}
+		}
+	}		
 }		
