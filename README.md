659fe720434b6c46ce70cbce47853621b606d8a4
gianni benavides <sb5021087@gmail.com>
2020-09-15 20:52:59 -0500

    inicializacion del programa

---
+++ README.md
@@ -0,0 +1,1 @@
+# Este programa es con el fin de ayudar en el manejo de precios de diversos productos
---
+++ src/Integradora.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Integradora{
+
+	public static final int CANTIDAD_MAXIMA = 100;
+	public static final String OBRA_NEGRA = "Obra negra";
+	public static final String OBRA_BLANCA = "Obra blanca";
+	public static final String OBRA_PINTURA = "Obra pintura";
+	
+	public static void main (String [] args) {
+		Scanner scan = new Scanner(System.in);
+		boolean run = true;
+	}	
+}		
