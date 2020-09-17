c6604b36b2cb03d1a2496b55cfec3324b8244716
gianni benavides <sb5021087@gmail.com>
2020-09-17 15:50:28 -0500

    Congigurado la Intencionalidad de la compra

--- src/Integradora.java
+++ src/Integradora.java
@@ -7,12 +7,14 @@ public class Integradora{
 	public static final String OBRA_BLANCA = "Obra blanca";
 	public static final String OBRA_PINTURA = "Obra pintura";
 	
+	
 	public static void main (String [] args) {
 		Scanner scan = new Scanner(System.in);
 		boolean run = true;
 		String[] nombres = new String[CANTIDAD_MAXIMA];
 		String[] intencion = new String[CANTIDAD_MAXIMA];
-		double[] precios = new double[CANTIDAD_MAXIMA];
+		double[] mejorPrecio = new double[CANTIDAD_MAXIMA];
+		String[] LugarMejorPrecio = new String[CANTIDAD_MAXIMA];
 		while(run) {
 			System.out.println("Escoja una opcion:");
 			System.out.println("1. Comparar una serie de productos y añadir a la lista el de menor precio.");
@@ -21,9 +23,10 @@ public class Integradora{
 			
 			int opcion =  Integer.parseInt(scan.nextLine());
 			switch(opcion) {
-			case 1: compararProductos(scan, nombres, precios);
+			case 1: compararProductos(scan, intencion , nombres, mejorPrecio,LugarMejorPrecio);
 					break;
-			case 2: mostrarEnPantalla( nombres, precios);
+			case 2: mostrarEnPantalla( nombres, mejorPrecio,LugarMejorPrecio,intencion);
+					
 					System.out.println();
 					break;
 			case 3: run = false;
@@ -36,7 +39,32 @@ public class Integradora{
 		
 	
 	}	
-	public static void compararProductos(Scanner scan, String[] nombres, double[] precios) {
+	public static String obtenerIntecionalidad(Scanner scan) {
+		boolean escogido = false;
+		String intencion = null;
+		while(!escogido) {
+			System.out.println("Escoja la opcion de la intencion con la obtiene el producto: ");
+			System.out.println("1. " + OBRA_BLANCA);
+			System.out.println("2. " + OBRA_NEGRA);
+			System.out.println("3. " + OBRA_PINTURA);
+			
+			int opcion =  Integer.parseInt(scan.nextLine());
+			switch(opcion) {
+			case 1: intencion = OBRA_BLANCA;
+					escogido = true;
+					break;
+			case 2: intencion = OBRA_NEGRA;
+					escogido = true;
+					break;
+			case 3: intencion = OBRA_PINTURA;
+					escogido = true;
+					break;
+			default:System.out.println("Opcion no valida, por favor escoja una opcion correcta.");
+			}
+		}
+		return intencion;
+	}
+	public static void compararProductos(Scanner scan,String[] intencion, String[] nombres, double[] mejorPrecio,String[] LugarMejorPrecio) {
 		System.out.println("Compara los productos:");
 		System.out.println("Cuantos productos ingresaras para comparar?");
 		
@@ -68,42 +96,53 @@ public class Integradora{
 			FerreteríaDelBarrio[i] = precioUnitario * Unidades;
 			
 			
-			//compararIntencionalidad[i] =  obtenerIntecionalidad(scan);
+			compararIntencionalidad[i] =  obtenerIntecionalidad(scan);
 		}
 		
-		add(HomeCenter , FerreteriaCentro , FerreteríaDelBarrio , compararNombre , nombres , precios , cantidad);
+		ComparacionValores(intencion ,compararIntencionalidad , HomeCenter , FerreteriaCentro , FerreteríaDelBarrio , compararNombre , nombres , mejorPrecio , cantidad , LugarMejorPrecio);
 	}
-	public static void add(double[] HomeCenter, double[]FerreteriaCentro , double[] FerreteríaDelBarrio ,String[] compararNombre 
-			, String[] nombres, double[] precios , int cantidad) {
+	public static void ComparacionValores(String[] intencion ,String[] compararIntencionalidad,double[] HomeCenter, double[]FerreteriaCentro , double[] FerreteríaDelBarrio ,String[] compararNombre 
+			, String[] nombres, double[] mejorPrecio , int cantidad,String[] LugarMejorPrecio) {
 				
 		double menorValor = 0;
 		
-		
+		String HomeC = " en Homcenter";
+		String Ferrec = " en Ferreteria centro ";
+		String FerreB = "en Ferreteria del barrio";
 		for (int i = 0; i < cantidad; i++) {
 			if((HomeCenter[i] < FerreteriaCentro[i]) &&(HomeCenter[i] < FerreteríaDelBarrio[i])) {
-				precios[i]= HomeCenter[i];
+				mejorPrecio[i]= HomeCenter[i];
+				LugarMejorPrecio[i]= HomeC ;
+				intencion[j]=compararIntencionalidad[i];
 			}
 			else
 			{
 				if((FerreteriaCentro[i]< HomeCenter[i])&&(FerreteriaCentro[i]<FerreteríaDelBarrio[i])){
-					precios[i]=FerreteriaCentro[i];
+					mejorPrecio[i]=FerreteriaCentro[i];
+					LugarMejorPrecio[i]= Ferrec;
+					intencion[i]=compararIntencionalidad[i];
 				} 
+				
 				else
 				{
 					if((FerreteríaDelBarrio[i]< HomeCenter[i])&&(FerreteriaCentro[i]>FerreteríaDelBarrio[i])){
-						precios[i]=FerreteríaDelBarrio[i];
+						mejorPrecio[i]=FerreteríaDelBarrio[i];
+						LugarMejorPrecio[i]= FerreB;
+						intencion[i]=compararIntencionalidad[i];
+						
 					}
 				}
 			}
 		}
 		
 	}
-	public static void mostrarEnPantalla ( String[] nombres, double[] precios ) {
+	public static void mostrarEnPantalla ( String[] nombres, double[] precios,String[] LugarMejorPrecio,String[] intencion ) {
 		int x = 1;
 		boolean completo = false;
+		System.out.println("Los mejores precios y sus puntos de venta son:");
 		for (int i = 0; i < precios.length && !completo; i++) {
 			if(nombres[i] != null) {
-				System.out.println(x + ". Nombre: " + nombres[i] + ". Precio: " + precios[i] + " Intencion: " );
+				System.out.println(x + ". Nombre: " + nombres[i] + ". Precio: " + precios[i] + " Lugar de compra " +LugarMejorPrecio[i] +"  "+  intencion[i]);
 				x++;
 			}else {
 				completo=true;
